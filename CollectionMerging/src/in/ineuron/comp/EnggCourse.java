0a709744e1a1ae9f9f537e5f30ece884db852b3a
Chandan Kumar <85821143+ChandanArya20@users.noreply.github.com>
2023-09-30 12:40:25 +0530

    Add files via upload

---
+++ CollectionMerging/src/in/ineuron/comp/EnggCourse.java
@@ -0,0 +1,24 @@
+package in.ineuron.comp;
+
+import java.util.Set;
+
+public class EnggCourse {
+
+	private Set<String> subjects;
+
+	public Set<String> getSubjects() {
+		return subjects;
+	}
+
+	public void setSubjects(Set<String> subjects) {
+		this.subjects = subjects;
+	}
+
+	@Override
+	public String toString() {
+		return "EnggCourse [subjects=" + subjects + "]";
+	}
+	
+	
+	
+}
